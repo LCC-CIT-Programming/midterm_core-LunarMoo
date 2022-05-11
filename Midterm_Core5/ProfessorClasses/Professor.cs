f16487399f42a47deb3ece49f9c75e3339990a41
LunarMoo <102759790+LunarMoo@users.noreply.github.com>
2022-05-10 19:12:47 -0700

    professor classes updated
    I was able to create a class with the info that is needed.

--- Midterm_Core5/ProfessorClasses/Professor.cs
+++ Midterm_Core5/ProfessorClasses/Professor.cs
@@ -4,5 +4,69 @@ namespace ProfessorClasses
 {
     public class Professor
     {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string teachingSubject;
+
+        public Professor(int profLn, string first, string last, string department)
+        {
+            id = profLn;
+            firstName = first;
+            lastName = last;
+            teachingSubject = department;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string TeachingSubject
+        {
+            get
+            {
+                return teachingSubject;
+            }
+            set
+            {
+                teachingSubject = value;
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("Id: {0} First Name: {1} Last Name: {2} Department:  {4}", id, firstName, lastName, teachingSubject);
+        }
     }
 }
+    
