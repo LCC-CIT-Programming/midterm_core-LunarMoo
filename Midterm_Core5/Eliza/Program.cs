84451ea44c17152bbb90a96e74bd443fa726edf0
LunarMoo <102759790+LunarMoo@users.noreply.github.com>
2022-05-10 19:09:47 -0700

    Eliza updated
    I not confident on this.

--- Midterm_Core5/Eliza/Program.cs
+++ Midterm_Core5/Eliza/Program.cs
@@ -6,7 +6,43 @@ namespace Eliza
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Eliza Response.");
+            Console.WriteLine();
+            Console.WriteLine("Please enter a Statement so Eliza can Respond.");
+            string input =  Console.ReadLine();
+
+            string answer = CreateElizaResponse(input);
+            Console.WriteLine("Response :" + answer);
         }
+
+        static bool IsVowel(char c)
+        {
+            switch (c)
+            {
+                
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'y':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        static string CreateElizaResponse(string s) 
+        {
+            string answer;
+            if (IsVowel(s[6]))
+                answer = "Tell me more";
+            else
+                answer = "You have strong feelings about that!";
+            return answer;
+        }
+
+
+
     }
 }
+
