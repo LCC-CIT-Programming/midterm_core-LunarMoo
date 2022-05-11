8b0f71fdda9db160cd70d4a1c14ba3b0aa95228a
LunarMoo <102759790+LunarMoo@users.noreply.github.com>
2022-05-10 19:11:18 -0700

    Palindrome Updated
    I did not complete it I add the original palindrome but I was not able to figure out how one was to make it where the punctuation was ignored.

--- Midterm_Core5/Palindrome/Program.cs
+++ Midterm_Core5/Palindrome/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections;
 
 namespace Palindrome
 {
     class Program
     {
+        public static bool IsPalindrome(string text)
+        {
+            if (text.Length <= 1)
+                return true;
+            else
+            {
+                if (text[0] != text[text.Length - 1])
+                    return false;
+                else
+                    return IsPalindrome(text.Substring(1, text.Length - 2));
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write(" Recursion : Check whether a string ia Palindrome or not :");
+
+            string str1;
+            bool tf;
+
+            Console.Write(" Input a string : ");
+            str1 = Console.ReadLine();
+            tf = IsPalindrome(str1);
+            if (tf == true)
+            {
+                Console.WriteLine(" The string is Palindrome.\n");
+            }
+            else
+            {
+                Console.WriteLine(" The string is not a Palindrome.\n");
+            }
+
+
         }
+
+
+        
     }
 }
+    
